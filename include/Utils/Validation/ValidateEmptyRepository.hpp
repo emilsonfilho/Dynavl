244676c2337ffd4ecf4bc3bbcc373e3ab87d81b1
meikilo <Emilson Filho>
2025-04-28 11:14:16 -0300

    (doc): validation function docs added

--- include/Utils/Validation/ValidateEmptyRepository.hpp
+++ include/Utils/Validation/ValidateEmptyRepository.hpp
@@ -4,6 +4,16 @@
 #include "Exceptions/EmptyRepositoryException.hpp"
 #include "Messages/EmptyRepositoryMessage.hpp"
 
+/**
+ * @brief Validates whether a repository is empty.
+ *
+ * This function checks if the repository size is zero. If it is, it throws
+ * an `EmptyRepositoryException` to indicate that operations cannot proceed
+ * on an empty repository.
+ *
+ * @param repositorySize The size of the repository to validate.
+ * @throws EmptyRepositoryException if the repository is empty.
+ */
 void ValidateEmptyRepository(size_t repositorySize);
 
 #endif
