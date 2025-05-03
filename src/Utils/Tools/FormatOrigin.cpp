635cc95f1007258b48cd6b8d669646d732679185
Emilson Filho <emilsonfilhocontato@gmail.com>
2025-05-03 15:07:13 -0300

    (feat): formatting the origins of new sets created by internal operations

---
+++ src/Utils/Tools/FormatOrigin.cpp
@@ -0,0 +1,6 @@
+#include "Utils/Tools/FormatOrigin.hpp"
+
+string formatOrigin(string op, int index1, int index2) {
+	string idx1 = to_string(index1), idx2 = to_string(index2);
+	return "fomado pela " + op + " entre os conjuntos " + idx1 " e " + idx2;
+}
