fecebb3a91fc2c4f84143908f788cd2e1c3161dc
Emilson Filho <emilsonfilhocontato@gmail.com>
2025-05-02 21:51:17 -0300

    (feat): new message added

---
+++ include/Messages/NoSuccessorMessage.hpp
@@ -0,0 +1,14 @@
+#ifndef NO_SUCCESSOR_MESSAGE_HPP
+#define NO_SUCCESSOR_MESSAGE_HPP
+
+#include <string>
+
+using std::string;
+using std::to_string;
+
+/**
+ * @brief Empty repository message
+ */
+string NoSuccessorMessage(int key);
+
+#endif
